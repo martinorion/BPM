4e212e2059fbce8bab17056220de49ea681a5cc3
Kristián Jakubik <martinkiko18@gmail.com>
2021-01-10 12:10:33 +0100

    MVVM

---
+++ BPM/BPM/Views/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using BPM.ViewModel;
+using Xamarin.Forms;
+
+namespace BPM
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly MainViewModel viewModel;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
